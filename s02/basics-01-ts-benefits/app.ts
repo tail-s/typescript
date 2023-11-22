b9de6b51bb66ab847d60cd6c13661010c4091492
tail-s <lunar7s@naver.com>
2023-11-22 17:30:08 +0900

    23.11.22

--- s02/basics-01-ts-benefits/app.ts
+++ s02/basics-01-ts-benefits/app.ts
@@ -1,9 +1,37 @@
-function add(n1: number, n2: number) {
-  return n1 + n2;
+// function add(n1: number, n2: number, showResult: boolean, phrase: string) {
+//   // if (typeof n1 !== 'number' || typeof n2 !== 'number') {
+//   //   throw new Error('Incorrect input!');
+//   // }
+//   const result = n1 + n2;
+//   if (showResult) {
+//     console.log(phrase + result);
+//   } else {
+//     return result;
+//   }
+// }
+
+// const number1 = 5;
+// const number2 = 2.8;
+// const printResult = true;
+// const resultPhrase = 'Result is: ';
+
+// add(number1, number2, printResult, resultPhrase);
+
+// const person: {
+//   name: string;
+//   age: number;
+// } = {
+const person = {
+  name: 'Sojin',
+  age: 28,
+  hobbies: ['Sports', 'Cooking']
+};
+
+let favoriteActivites: string[];
+favoriteActivites = ['Sports'];
+
+for (const hobby of person.hobbies) {
+  console.log(hobby);
 }
 
-const number1 = 5;
-const number2 = 2.8;
-
-const result = add(number1, number2);
-console.log(result);
+console.log(person.name);
